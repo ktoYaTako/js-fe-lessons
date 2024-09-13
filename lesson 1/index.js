580785039d8c3897a5b0a7ce4f6c57f8e548918f
ktoYaTako? <enotek221@gmail.com>
2024-09-13 13:17:55 +0300

    commit 4

--- lesson 1/index.js
+++ lesson 1/index.js
@@ -4,81 +4,79 @@ let time
 let price 
 
 function start () {
-    money = prompt('Ваш бюджет?');
+    money = prompt('Ваш бюджет?', '');
 
     while (isNaN(money) || money == '' || money == null) {
-        money = prompt('Ваш бюджет?'); 
+        money = prompt('Ваш бюджет?', ''); 
     }
 
-    name = prompt('Название вашего магазина?').toUpperCase();
+    name = prompt('Название вашего магазина?', '').toUpperCase();
     time = 19;
 }
 
-start()
+
+
 
 let mainList = {
     budget: money,
     shopName: name,
     shopGoods: [],
-    employers: [],
-    opent: false,
-    discount: false
-}
-
-function salesPrice (price) {
-    if (mainList.discount === true) {
-        price *= 0.8
-    }
-}
-
-salesPrice()
-
-function hireEmployer () {
-  for (let i = 0; i < 5; i++) {
-    let employ = prompt('Укажите номер и имя сотрудника');
-
-    if ((typeof(employ)) === `string`) {
-        console.log('Все верно!')
-        let employName =  RegExp(/^[A-Za-z]+$/, employ)
-        let employObj = {number: parseInt(employ), nameEmployer: employName}
-        mainList.employers.push(employObj)
-    } else {
-        i = i - 1
-    }
-  }
-}
-hireEmployer()
-
-function chooseGoods () {
-    for (let i = 0; i < 5; i++) {
-        let a = prompt('Какой тип товаров будем продавать?');
+    employers: {},
+    open: false,
+    discount: false,
+    shopItems: [],
+    chooseGoods : function chooseGoods () {
+        for (let i = 0; i < 5; i++) {
+            let a = prompt('Какой тип товаров будем продавать?', '');
+            
+            if ((typeof(a)) === 'string' && (typeof(a)) !== null && (typeof(a)) !== '' && a.length < 20) {
+                console.log('Все верно!')
+                mainList.shopGoods[i] = a;
+            } else {
+                i = i - 1
+            }
         
-        if ((typeof(a)) === 'string' && (typeof(a)) !== null && (typeof(a)) !== '' && a.length < 20) {
-            console.log('Все верно!')
-            mainList.shopGoods[i] = a;
-        } else {
-            i = i - 1
         }
-    
-    }
+    },
+    workTime: function workTime (time) {
+        if (time < 0) {
+            console.log('Ты долбаеб?')
+        } else if (time > 8 && time < 20) {
+            console.log('Время покупать, гнида!')
+            mainList.open = true;
+            }   else if (time < 24) {
+                console.log('Иди спать уебок, мы закрыты')
+        }           else {
+                    console.log('Ты долбаеб?')
+        }
+    },
+    dayBudget: function dayBudget () {
+        alert('Ежедневный бюджет ' + mainList.budget / 30)
+    },
+    makeDiscount: function makeDiscount (price) {
+        if (mainList.discount === true) {
+            price = (price / 100) * 80
+        }
+    },
+    hireEmployer: function hireEmployer () {
+        for (let i = 0; i < 5; i++) {
+          let name = prompt('Укажите имя сотрудника', '');
+          mainList.employers[i] = name
+        }
+      },
+      chooseShopItem: function chooseShopItem () {
+        let items = prompt('Перечислите через запятую товары', '');
+
+        mainList.shopItems = items.split(',');
+        mainList.shopItems.push(prompt('Подождите еще ',''))
+        mainList.shopGoods.sort();
+
+      }
 }
-
-chooseGoods()
-
-
-function workTime (time) {
-    if (time < 0) {
-        console.log('Ты долбаеб?')
-    } else if (time > 8 && time < 20) {
-        console.log('Время покупать, гнида!')
-        }   else if (time < 24) {
-            console.log('Иди спать уебок, мы закрыты')
-    }           else {
-                console.log('Ты долбаеб?')
-    }
-}
-workTime()
-
-
-alert(mainList.budget / 30)
 console.log(mainList)
+
+
+
+
+
+
